23df99bac9eb72844f7005bb642406ae5ae622ef
ritubajaj89 <88427398+ritubajaj89@users.noreply.github.com>
2022-03-03 10:29:18 +0530

    changing repo

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-apt-get update
+yum update -y
 
 if ! type "docker" > /dev/null; then
   echo "Installing Docker"
-  apt-get -y install docker.io
-  apt-get -y install docker-ce
+  yum -y install docker.io
+  yum -y install docker-ce
 fi
 
 # get ips | search inet addr: | split on : get 2nd field | print out
 ADDRESS=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 # if we have an emtpy address
-if [[ -z "${ADDRESS}"]]; then
+if [ -z "${ADDRESS}"]; then
   # some prints won't have the addr portion, do a last ditch effort to get the ip
   # get ips | search inet | get first line | trim string | split on ' ' get the 2nd field | print out
   ADDRESS=$(ifconfig eth0 | grep 'inet' | awk 'NR == 1' | awk '{$1=$1};1' | cut -d ' ' -f 2 | awk '{ print $1}')
@@ -19,15 +19,15 @@ docker swarm init --advertise-addr="$ADDRESS"
 
 if ! type "git" > /dev/null; then
   echo "Installing Git"
-  apt-get -y install git
+  yum -y install git
 fi
 
-DIRECTORY="prometheus-grafana-alertmanager-example"
+DIRECTORY="prom-ami-testing"
 if [ -d "$DIRECTORY" ]; then
   rm -rf "$DIRECTORY"
 fi
 echo "Cloning Project"
-git clone https://github.com/PagerTree/prometheus-grafana-alertmanager-example.git
+git clone https://github.com/ritubajaj89/prom-ami-testing.git
 cd "$DIRECTORY"
 
 echo "Making Utility scripts executable"
